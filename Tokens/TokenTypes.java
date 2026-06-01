9c2e6c87bed2e119deb4befd1dec9993d289f18c
ErrorVXZ <72672251+ErrorVXZ@users.noreply.github.com>
2026-06-02 00:37:07 +0200

    Create TokenTypes.java

---
+++ Tokens/TokenTypes.java
@@ -0,0 +1,17 @@
+public class TokenTypes {
+ 
+    public static final int KLASSE     = 1;
+    public static final int TYP        = 2;
+    public static final int WENN       = 14;
+    public static final int SONST      = 15;
+
+  
+    public static final int BLOCKAUF   = 3;
+    public static final int BLOCKZU    = 4;
+    public static final int SEMIKOLON  = 5;
+    public static final int ZUWEISUNGSOP = 8;
+
+    
+    public static final int ZAHL       = 11;
+    public static final int NAME       = 12;
+}

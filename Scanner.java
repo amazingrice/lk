c2592ac63cf17bb856fbe99850bd7756d25144f4
amazingrice <kmduyar@gmail.com>
2026-06-09 09:21:33 +0200

    Add initial Scanner class with input handling

--- Scanner.java
+++ Scanner.java
@@ -1,1 +1,26 @@
+import java.util.List
+  public Class Scanner {
+    public String input;
+    public int position = 0; 
 
+  public String Scanne (String input){
+    
+    
+
+
+
+
+    
+  }
+    
+    
+    
+
+
+
+
+
+
+
+    
+  }
